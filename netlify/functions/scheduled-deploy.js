259ed6db75ff4d0152053d79102c91997797d749
Ivan <ivan@krikstudio.com>
2022-10-23 11:38:42 +0200

    redeploy test

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -6,7 +6,7 @@ import { schedule } from '@netlify/functions'
 // This is sample build hook
 const BUILD_HOOK = 'https://api.netlify.com/build_hooks/63528ae9386d020f6e1d349b'
 
-const handler = schedule('35 11 * * 1-6', async () => {
+const handler = schedule('42 11 * * *', async () => {
     await fetch(BUILD_HOOK, {
       method: 'POST'
     }).then(response => {
