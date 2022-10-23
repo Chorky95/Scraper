992268496da4c4367811739ad746f0e74f3a7300
Ivan <ivan@krikstudio.com>
2022-10-23 16:50:38 +0200

    bugfix

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -1,6 +1,6 @@
 //https://api.netlify.com/build_hooks/63528ae9386d020f6e1d349b
 
-const fetch = require('node-fetch')
+import fetch from 'node-fetch'
 import { schedule } from '@netlify/functions'
 
 // This is sample build hook
