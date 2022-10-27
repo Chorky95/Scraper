7b60b07f8eb189a4ac3976a3d86651efcad55f46
Ivan <ivan@krikstudio.com>
2022-10-28 00:03:54 +0200

    scheduling fix

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -5,7 +5,7 @@ const BUILD_HOOK = "https://api.netlify.com/build_hooks/63550d353255bf5920840f06
 
 console.log('Starting scheduled deployment...')
 
-const handler = schedule('* * * * *', async () => {
+const handler = schedule('* 7 * * 1-6', async () => {
     await fetch(BUILD_HOOK, {
         method: 'POST'
       }).then((response) => {
